fe1c7f5352656758670456b7fcaace15f436d396
Boyuan Zhang <alexander.boyuan.zhang@gmail.com>
2020-10-31 16:32:59 +0100

    .

--- com/runtime/GetMemory.java
+++ com/runtime/GetMemory.java
@@ -1,3 +1,4 @@
+package com.runtime;
 /**
  * @description: Prints JVM memory utilization statistics
  */
--- com/xml/XSDValidator.java
+++ com/xml/XSDValidator.java
@@ -1,4 +1,4 @@
-package xml;
+package com.xml;
 import java.io.File;
 import java.io.IOException;
 
